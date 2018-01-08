db8ab5ed8b8893e07d2f53a882f24e82d44d57e5
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-08 10:38:52 -0800

    using interceptors.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,19 @@ import router from './router'
 import store from './store'
 
 axios.defaults.baseURL = "https://vuejs-axios-7909e.firebaseio.com"
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+const reqInt = axios.interceptors.request.use(config => {
+  console.log('request: ' + JSON.stringify(config, null, 2))
+  return config
+})
+const respInt = axios.interceptors.response.use(res => {
+  console.log('response: ' + JSON.stringify(res, null, 2))
+  return res
+})
+
+axios.interceptors.request.eject(reqInt)
+axios.interceptors.response.eject(respInt)
 
 new Vue({
   el: '#app',
