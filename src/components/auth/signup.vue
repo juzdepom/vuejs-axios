e19187cb871d0c56a6a97cf9b4d2454267c4b0c8
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-09 22:55:15 -0800

    Sending the token to the backend.

--- src/components/auth/signup.vue
+++ src/components/auth/signup.vue
@@ -103,7 +103,7 @@
           terms: this.terms
         }
         console.log(formData)
-        this.$store.dispatch('signUp', {email: formData.email, password: formData.password})
+        this.$store.dispatch('signUp', formData)
       }
     }
   }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from './axios-auth'
+import globalAxios from 'axios'
 
 Vue.use(Vuex)
 
@@ -8,19 +9,41 @@ export default new Vuex.Store({
   state: {
     idToken: null,
     userId: null,
+    user: null,
   },
   mutations: {
-
+    authUser (state, userData) {
+      state.idToken = userData.token
+      state.userId = userData.userId
+    },
+    storeUser (state, userData ) {
+      state.user = userData
+    }
   },
   actions: {
-    signUp({ commit }, authData){
+    signUp({ commit, dispatch }, formData){
       axios.post('/signupNewUser?key=AIzaSyCa23LBECfmUKFzFe5oOBDJbCabnycYPbU', {
-        email: authData.email,
-        password: authData.password,
+        email: formData.email,
+        password: formData.password,
         returnSecureToken: true,
         })
-        .then(res => { console.log(res) })
+        .then(res => {
+          console.log(res)
+          commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId,
+          })
+        })
         .catch(error => { console.log(error) })
+        dispatch('storeUser', formData )
+    },
+    storeUser({ commit, state }, userData){
+      if(!state.idToken){
+        return
+      }
+      globalAxios.post('/users.json' + "?auth=" + state.idToken, userData)
+        .then(res => console.log(response))
+        .catch(error => console.log('ERROR! ', error))
     },
     signIn({ commit }, authData){
       axios.post('/verifyPassword?key=AIzaSyCa23LBECfmUKFzFe5oOBDJbCabnycYPbU', {
@@ -28,11 +51,39 @@ export default new Vuex.Store({
         password: authData.password,
         returnSecureToken: true,
         })
-        .then(res => { console.log(res) })
+        .then(res => {
+          console.log(res)
+          commit('authUser', {
+            token: res.data.idToken,
+            userId: res.data.localId,
+          })
+        })
         .catch(error => { console.log("ERROR! ", error) })
+    },
+    fetchUser({ commit, state }){
+        if(!state.idToken){
+          console.log('no id token')
+          return
+        }
+        globalAxios.get('/users.json' + "?auth=" + state.idToken)
+        .then(res => {
+          console.log(res.data)
+          const data = res.data
+          const users = []
+          for (let key in data) {
+            const user = data[key]
+            user.id = key
+            users.push(user)
+          }
+          console.log(users)
+          commit('storeUser', users[0])
+        })
+        .catch(error => {console.log(error)})
     }
   },
   getters: {
-
+    user (state) {
+      return state.user;
+    }
   }
 })
